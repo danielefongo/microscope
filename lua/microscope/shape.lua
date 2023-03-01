d36730900baf0e1bd0f4d5f2d405153b35eb83b8
danielefongo <danielefongo@gmail.com>
2023-03-02 00:17:31 +0100

    minor improvements on shape

--- lua/microscope/shape.lua
+++ lua/microscope/shape.lua
@@ -1,13 +1,15 @@
 local shape = {}
 
+local OFFSET = 2
+
 local function absolute(size)
   local ui = vim.api.nvim_list_uis()[1]
   return {
     relative = "editor",
     width = size.width,
     height = size.height,
-    col = (ui.width / 2) - (size.width / 2),
-    row = (ui.height / 2) - (size.height / 2),
+    col = (ui.width / 2) - (size.width / 2) - 1,
+    row = (ui.height / 2) - (size.height / 2) - 1,
     style = "minimal",
     border = "rounded",
   }
@@ -24,9 +26,16 @@ end
 
 local function horizontal(size, has_preview)
   local input_height = 1
-  local input_offset = input_height + 2
-  local results_height = size.height - input_offset
-  local left_width = (has_preview and size.width / 2 - 2) or size.width
+  local results_height = size.height - input_height - OFFSET
+
+  local left_width
+  local preview_width
+  if has_preview then
+    left_width = math.floor(size.width / 2) - OFFSET
+    preview_width = size.width - left_width - OFFSET
+  else
+    left_width = size.width
+  end
 
   local input_opts = relative(size, {
     x = 0,
@@ -36,15 +45,15 @@ local function horizontal(size, has_preview)
   })
   local results_opts = relative(size, {
     x = 0,
-    y = input_offset,
+    y = input_height + OFFSET,
     width = left_width,
     height = results_height,
   })
   local preview_opts = has_preview
     and relative(size, {
-      x = size.width / 2,
+      x = left_width + OFFSET,
       y = 0,
-      width = size.width / 2,
+      width = preview_width,
       height = size.height,
     })
   return {
@@ -55,31 +64,34 @@ local function horizontal(size, has_preview)
 end
 
 local function vertical(size, has_preview)
-  local width = vim.api.nvim_list_uis()[1].width
-
   local input_height = 1
-  local input_offset = input_height + 2
-  local rest_height = size.height - input_offset
-  local results_height = (has_preview and math.floor(rest_height / 2) + 1) or rest_height
+  local results_height
+  local preview_height
+  if has_preview then
+    results_height = math.floor((size.height - input_height - OFFSET) / 2) - OFFSET
+    preview_height = size.height - results_height - input_height - 2 * OFFSET
+  else
+    results_height = size.height - input_height - OFFSET
+  end
 
   local input_opts = relative(size, {
     x = 0,
     y = 0,
-    width = width,
+    width = size.width,
     height = input_height,
   })
   local results_opts = relative(size, {
     x = 0,
-    y = input_offset,
-    width = width,
+    y = input_height + OFFSET,
+    width = size.width,
     height = results_height,
   })
   local preview_opts = has_preview
     and relative(size, {
       x = 0,
-      y = input_offset + results_height + 2,
-      width = width,
-      height = size.height - input_height - results_height,
+      y = input_height + results_height + 2 * OFFSET,
+      width = size.width,
+      height = preview_height,
     })
   return {
     input = input_opts,
@@ -90,10 +102,16 @@ end
 
 function shape.generate(size, has_preview)
   local ui = vim.api.nvim_list_uis()[1]
+
+  local real_size = {
+    width = math.min(size.width, ui.width - 4),
+    height = math.min(size.height, ui.height - 4),
+  }
+
   if ui.width < size.width then
-    return vertical(size, has_preview)
+    return vertical(real_size, has_preview)
   else
-    return horizontal(size, has_preview)
+    return horizontal(real_size, has_preview)
   end
 end
 
