43db38a8aab5055791360a700d3d3166342de0fd
danielefongo <danielefongo@gmail.com>
2023-03-06 08:52:02 +0100

    feat: preview binary and too big files

--- lua/microscope/files/init.lua
+++ lua/microscope/files/init.lua
@@ -5,6 +5,35 @@ local highlight = require("microscope.files.highlight")
 
 local files = {}
 
+files.max_size = 2 ^ 16
+
+local function exists(path)
+  local file = vim.loop.fs_open(path, "r", 438)
+  if file == nil then
+    return false
+  end
+  vim.loop.fs_close(file)
+  return true
+end
+
+local function too_big(path)
+  local file = vim.loop.fs_open(path, "r", 438)
+  local file_size = vim.loop.fs_fstat(file).size
+  vim.loop.fs_close(file)
+  return file_size > files.max_size
+end
+
+local function is_binary(path)
+  local handle = io.popen(string.format("file -n -b --mime-encoding '%s'", path))
+  if not handle then
+    return false
+  end
+  local binary_file = handle:read("*a"):match("binary")
+
+  handle:close()
+  return binary_file
+end
+
 function files.open(data, win, _)
   vim.cmd("e " .. data.text)
   if data.row and data.col then
@@ -20,13 +49,27 @@ function files.preview(data, win, buf)
   else
     cursor = { 1, 0 }
   end
+
+  if not exists(data.text) then
+    vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "Not existing" })
+    return
+  end
+
+  if is_binary(data.text) then
+    vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "Binary" })
+    return
+  end
+
+  if too_big(data.text) then
+    vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "Too big" })
+    return
+  end
+
   if files.stream then
     files.stream:stop()
   end
-  files.stream = stream.chain({
-    files_lists.cat(data.text),
-    lists.head(5000),
-  }, function(lines)
+
+  files.stream = stream.chain({ files_lists.cat(data.text) }, function(lines)
     vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
     highlight(data.text, buf)
     vim.api.nvim_win_set_cursor(win, cursor)
