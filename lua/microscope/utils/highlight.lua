84a6cb6fb0f02fc385e8a72b2c62a8d89b6eaf1e
danielefongo <danielefongo@gmail.com>
2023-05-26 19:25:04 +0200

    fix: set buffer highlights properly when using utils.highlight

--- lua/microscope/utils/highlight.lua
+++ lua/microscope/utils/highlight.lua
@@ -1,20 +1,14 @@
 return function(path, buf)
-  vim.api.nvim_buf_set_name(buf, vim.fn.tempname() .. "/" .. path)
-  vim.api.nvim_buf_call(buf, function()
-    local eventignore = vim.api.nvim_get_option("eventignore")
-    vim.api.nvim_set_option("eventignore", "FileType")
-    vim.api.nvim_command("filetype detect")
-    return vim.api.nvim_set_option("eventignore", eventignore)
-  end)
-  local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-
-  if not ft or ft == "" then
-    return
-  end
+  local ft = vim.filetype.match({ filename = path })
 
   if pcall(require, "nvim-treesitter") then
     local highlight = require("nvim-treesitter.highlight")
     local parsers = require("nvim-treesitter.parsers")
+
+    highlight.detach(buf)
+    if not ft or ft == "" then
+      return
+    end
 
     local lang = parsers.ft_to_lang(ft)
     if parsers.has_parser(lang) then
