14d0c95a7e2ea91626910048f187832fb4b76235
danielefongo <danielefongo@gmail.com>
2023-05-26 10:16:22 +0200

    fix: show preview on small UIs on default layout

--- lua/microscope/builtin/layouts.lua
+++ lua/microscope/builtin/layouts.lua
@@ -2,14 +2,9 @@ local display = require("microscope.api.display")
 local layouts = {}
 
 function layouts.default(opts)
-  local size
-  if opts.full_screen then
-    size = opts.ui_size
-  else
-    size = opts.finder_size
-  end
+  local size = (opts.full_screen and opts.ui_size) or opts.finder_size
 
-  if opts.ui_size.width < opts.finder_size.width or not opts.preview then
+  if not opts.preview then
     return display
       .horizontal({
         display.space("20%"),
@@ -17,6 +12,10 @@ function layouts.default(opts)
         display.space("20%"),
       })
       :build(size)
+  end
+
+  if opts.ui_size.width < opts.finder_size.width then
+    return display.vertical({ display.input(1), display.results(), display.preview() }):build(size)
   else
     return display
       .horizontal({
