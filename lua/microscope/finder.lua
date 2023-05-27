9158139203df9d21a7fbaa0d0b6e06af62c10ed7
danielefongo <danielefongo@gmail.com>
2023-05-27 11:16:57 +0200

    fix: reset cursor position after closing prompt

--- lua/microscope/finder.lua
+++ lua/microscope/finder.lua
@@ -32,8 +32,12 @@ end
 
 function finder:close()
   events.clear_module(self)
+
+  local cursor = vim.api.nvim_win_get_cursor(self.old_win)
   vim.api.nvim_set_current_win(self.old_win)
   vim.api.nvim_set_current_buf(self.old_buf)
+  vim.api.nvim_win_set_cursor(self.old_win, { cursor[1], cursor[2] + 1 })
+
   events.fire(events.event.microscope_closed)
   self:stop_search()
   finder.instance = nil
